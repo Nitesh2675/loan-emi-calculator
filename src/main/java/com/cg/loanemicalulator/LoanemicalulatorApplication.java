39e5f072c1dfc6e58f4fbdd7168f37a1ad191861
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-05-01 21:42:24 +0530

    [Nitesh] ADD: initial commit

---
+++ src/main/java/com/cg/loanemicalulator/LoanemicalulatorApplication.java
@@ -0,0 +1,13 @@
+package com.cg.loanemicalulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanemicalulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanemicalulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/loanemicalulator/LoanemicalulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.loanemicalulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanemicalulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
